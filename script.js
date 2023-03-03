012cf3823406d1a8fb524fb165b2ded4756a91d9
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-03-03 20:59:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+const input = document.querySelector(`#evaluatedText`);
+const h3 = document.querySelector(`#wordCount`);
+
+function countWords(e) {
+  e.preventDefault();
+
+  const inputVal = input.value;
+  let arr = inputVal.split(` `);
+
+  h3.textContent = arr.length;
+}
+
+input.addEventListener(`input`, countWords);
